26534f33c2436097490939b757e8fff567301f51
David Mbah <davidmbah41@gmail.com>
2023-03-17 06:22:27 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,16 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num <= 10; num++)
 	{
 		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
-
 	return (0);
 }
