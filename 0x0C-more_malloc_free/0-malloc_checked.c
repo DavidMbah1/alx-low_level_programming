9c8b40569469656f11f461e5c723b0d7d3f11099
David Mbah <davidmbah41@gmail.com>
2023-04-13 14:19:33 -0700

    Task 1 on malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - cause normal process termination with a status value of 98
+ * @b: this is the allocated memory
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *k
+
+	k = malloc(b);
+	if (k == NULL)
+		exit(98);
+	return (k);
+
+}
