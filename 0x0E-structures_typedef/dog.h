316657ec5e59819a48231a03877b9fd5733de86e
David Mbah <davidmbah41@gmail.com>
2023-04-18 02:53:31 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG
+#define DOG
 
 /**
  * struct dog - this is the dog class
@@ -8,11 +8,23 @@
  * @owner: Owner of the dog
  */
 
-struct dog{
-
-	char* name;
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
-#endif 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+
+typedef struct dog dog_t;
+
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+
+void free_dog(dog_t *d);
+
+#endif
