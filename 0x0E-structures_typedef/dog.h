c5af7b0ccc6e6097463ad73c18e1970dfe9a1f3b
David Mbah <davidmbah41@gmail.com>
2023-04-18 02:49:52 -0700

    task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - this is the dog class
+ * @name: name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
+struct dog{
+
+	char* name;
+	float age;
+	char* owner;
+};
+
+#endif 
